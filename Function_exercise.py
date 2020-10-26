2eb726a598f2d32148dc12908c3c6d274e2fd904
Alex-Smith <smithalexanderbradley@gmail.com>
2020-10-26 12:30:44 +0000

    fixed issue with function

--- Function_exercise.py
+++ Function_exercise.py
@@ -2,7 +2,7 @@ def grade(name, hw_score, assess_score, final_exam):
     hw_percent = hw_score * 4
     assess_percent = assess_score * 2
     total_bf_divide = hw_percent + assess_percent + final_exam
-    total = total_bf_divide/4
+    total = total_bf_divide/3
     if total >= 70:
         grade = 'A' 
     elif total >= 60:
@@ -13,7 +13,7 @@ def grade(name, hw_score, assess_score, final_exam):
         grade = 'D'
     else: 
         grade = 'Fail'
-    return name, grade    
+    return name, total, grade    
 
-grade1 = grade('Alex', 23, 44, 80)
+grade1 = grade('Alex', 20, 44, 90)
 print(grade1)    
