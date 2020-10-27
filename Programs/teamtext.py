354ba82c7f585a406f5fe2a8eb2fae1a03a6cc78
Alex-Smith <smithalexanderbradley@gmail.com>
2020-10-27 14:02:58 +0000

    Added gitlab exercises

---
+++ Programs/teamtext.py
@@ -0,0 +1,13 @@
+
+
+file = open("filename.txt", "r")
+
+outfile = ""
+
+for line in range(1, 6):
+    if line == 1 or line == 4
+        outfile += file.readline()
+    else:
+        file.readline()
+print(outfile)
+file.close()
