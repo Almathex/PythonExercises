bd97b8b5ae641d997c30fdbfe5f524e7427f2155
Alex-Smith <smithalexanderbradley@gmail.com>
2020-10-27 14:21:13 +0000

    tried to fix issue

--- Programs/teamtext.py
+++ Programs/teamtext.py
@@ -1,8 +1,8 @@
 file = open("teams.txt", "w")
 
 club = ('test', 'Chrome', 'Atlas', 'Whipsnakes', 'Archers', 'Chaos')
+file.seek(0)
 for i in range (1,6):
-    file.seek(0)
     newline = club[i] + " Lacrosse club /n "
     file.write(newline)
 
