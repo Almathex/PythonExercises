642c8fcf1ee952db7cdc920e6db2111dc878c325
Alex-Smith <smithalexanderbradley@gmail.com>
2020-10-27 11:51:13 +0000

    Tidied up file explorer

--- Route_planner.py
+++
@@ -1,9 +0,0 @@
-numblist = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
-for i in numblist:
-    try:
-        if numblist[i] > numblist[i + 1]:
-            continue
-        else:
-            print(i)
-    except:
-        pass
