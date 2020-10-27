e38b8f2ce1e9cf7b7231e3107e15bdc4cf76a1a7
Alex-Smith <smithalexanderbradley@gmail.com>
2020-10-27 14:25:05 +0000

    Added a team.txt file properly

--- Programs/teamtext.py
+++ Programs/teamtext.py
@@ -3,7 +3,7 @@ file = open("teams.txt", "w")
 club = ('test', 'Chrome', 'Atlas', 'Whipsnakes', 'Archers', 'Chaos')
 file.seek(0)
 for i in range (1,6):
-    newline = club[i] + " Lacrosse club \n "
+    newline = club[i] + " Lacrosse club \n"
     file.write(newline)
 
 file.close()    
